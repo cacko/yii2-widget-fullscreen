f1997b2246d640db1bb2988b8450ca43471153ab
Cac Ko <jago@mutanti.com>
2021-03-31 10:54:26 +0100

    fak

---
+++ README.md
@@ -0,0 +1,23 @@
+# yii2-widget-fullscreen
+
+An a shite straightfoward way to wrap your junk html containeirs in yii2 with a fullscreen functionality
+
+## Demo
+* demo - https://cacko-yii2.herokuapp.com/fullscreen/widget
+* src - https://github.com/cacko/yii2-widgets/tree/master/modules/fullscreen
+
+## Install
+```shell
+composer install cacko/yii2-widget-fullscreen
+```
+
+## Usage
+
+```php
+use Cacko\Yii2\Widgets\FullScreen\widgets\FullScreenWidget;
+
+echo FullScreenWidget::widget(['content' => Html::img('/start.webp')]);
+
+```
+
+there's bunch of defaults in `Cacko\Yii2\Widgets\FullScreen\components\FullScreenDefaults`, as usual if you think they are shit, feel free to inject your own bollocks, just keep in mind you will have to bother with the css styles too - good luck
---
+++ src/widgets/FullScreenWidget.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Cacko\Yii2\Widgets\FullScreen\widgets;
+
+use Cacko\Yii2\Widgets\FullScreen\components\Options;
+use Cacko\Yii2\Widgets\FullScreen\FullScreenAsset;
+use yii\base\Widget;
+use yii\helpers\Html;
+
+class FullScreenWidget extends Widget
+{
+
+    public string $content;
+
+    protected Options $defaults;
+
+    public function __construct(Options $defaults, $config = [])
+    {
+        $this->defaults = $defaults;
+        parent::__construct($config);
+    }
+
+    public function run()
+    {
+
+        $defaults = $this->defaults;
+
+        echo Html::beginTag('div', ['class' => ['fullscreen-widget'], 'id' => $this->id]);
+
+        echo Html::tag('div',  $this->renderIcon()  . $this->content, ['class' => $defaults->classFullScreen]);
+
+        echo Html::endTag('div');
+
+        FullScreenAsset::registerWidget($this);
+    }
+
+    protected function renderIcon()
+    {
+        $defaults = $this->defaults;
+        return Html::tag('div', Html::tag('i', '', ['class' => $defaults->iconExpand]), ['class' => $defaults->classToggle]);
+    }
+}
